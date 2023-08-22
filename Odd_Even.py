01848a5ac8f65854bfe3486f513edd2d688ccf10
Rovic Villaralvo <102729133+kazewanderboy@users.noreply.github.com>
2023-08-22 09:05:01 +0800

    Update Final

--- Odd_Even.py
+++ Odd_Even.py
@@ -1,8 +1,9 @@
 print("Determine whether an input\nnumber is even or odd\n")
 
+
+#This function check whether an input is odd or even
 def determine_odd_even(input_number):
     return "Even" if input_number % 2 == 0 else "Odd"
-
 
 while True:
     input_number = input("Enter a number: ")
